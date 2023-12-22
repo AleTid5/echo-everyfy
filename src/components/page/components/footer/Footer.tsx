099d7b3e219f80ca536ba3e11b44cbbeaa1d1568
alejandro.tidele <alejandro.tidele@glovoapp.com>
2023-12-22 11:21:15 +0100

    Update Footer

--- src/components/page/components/footer/Footer.tsx
+++ src/components/page/components/footer/Footer.tsx
@@ -3,8 +3,9 @@ const Footer = () => {
 
   return (
     <footer className="flex items-center justify-center py-4 bg-neutral-200 dark:bg-neutral-600">
-      <span className="text-sm text-gray-500 dark:text-gray-400">
-        © {year} EchoEveryfy by Alejandro Tidele. All rights reserved.
+      <span className="text-sm text-gray-500 dark:text-gray-400 text-center">
+        © {year} EchoEveryfy by Alejandro Tidele. <br className="sm:hidden" />
+        All rights reserved.
       </span>
     </footer>
   );
